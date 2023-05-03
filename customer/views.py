29f1b5808e7ae63c9d0c9d2604a8a8029f992527
swiffftttttssss <99203119+swiffftttttssss@users.noreply.github.com>
2023-05-02 20:59:42 -0500

    Delete customer directory

--- customer/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-class Index(View):
-    def get(self, request, *args, **kwargs):
-        return render(request,'customer/index.html')
-
-class About(View):
-    def get(self, request, *args, **kwargs):
-        return render(request,'customer/about.html')
