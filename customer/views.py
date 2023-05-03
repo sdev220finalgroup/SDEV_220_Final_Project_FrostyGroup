b7258c0a048a204754d9d02a4d8bc59630bf50d3
swiffftttttssss <99203119+swiffftttttssss@users.noreply.github.com>
2023-05-02 21:01:32 -0500

    Add files via upload

---
+++ customer/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from django.views import View
+from .models import MenuItem, Category, OrderModel
+
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/index.html')
+
+
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/about.html')
+
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        # get every item from each category
+        cones = MenuItem.objects.filter(category__name__contains='Cups')
+        shakes = MenuItem.objects.filter(category__name__contains='Shakes')
+        cups = MenuItem.objects.filter(category__name__contains='Cones')
+
+        # pass into context
+        context = {
+            'cones': cones,
+            'shakes': shakes,
+            'cups': cups,
+        }
+
+        # render the template
+        return render(request, 'customer/order.html', context)
+
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0
+        item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+
+        order = OrderModel.objects.create(price=price)
+        order.items.add(*item_ids)
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return render(request, 'customer/order_confirmation.html', context)
